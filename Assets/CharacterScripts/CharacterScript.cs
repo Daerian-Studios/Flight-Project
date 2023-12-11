d5cdb1f7c40316af88cc17a3b48c4c3a88d199fe
Docheru Florin-Andrei <andrei.docheru@yahoo.com>
2023-12-11 22:50:55 +0200

    Working!(Pre-Pre-Alpha)

--- Assets/CharacterScripts/CharacterScript.cs
+++ Assets/CharacterScripts/CharacterScript.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class CharacterScript : MonoBehaviour
 {
     GameObject Character;
-    float velocity = 2, acceleration = 0;
+    public float velocityUp = 1, velocityForward = 20, airResistance = 1.5f;
+    public CharacterWingsScript wings;
     // Start is called before the first frame update
     void Start()
     {
         Character = GameObject.Find("testCharacter");
+
+        wings = FindObjectOfType<CharacterWingsScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Character.transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+        Character.transform.Translate(Vector3.up * velocityUp * Time.deltaTime);
+        Character.transform.Translate(Vector3.forward * velocityForward * Time.deltaTime);
+        velocityUp -= airResistance * Time.deltaTime;
     }
 }
